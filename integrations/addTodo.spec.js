b802c845384492d8659f251dce1e360b4de16f2b
AnuchitO <anuchit.prasertasng@gmail.com>
2021-06-20 18:35:01 +0700

    refactor

--- integrations/addTodo.spec.js
+++ integrations/addTodo.spec.js
@@ -3,13 +3,11 @@ const env = require("../src/env")
 
 describe("Integration Test", () => {
     test('it stores item correctly', async () => {
-        const host = env.apiBaseURL
-        const port = 3000
         const path = '/items'
         const method = 'POST'
         const payload = { name: "What's up!! anuchito" }
 
-        const res = await request(host, port, path, method, payload)
+        const res = await request(env.apiBaseURL, env.apiPort, path, method, payload)
 
         expect(res.body).toHaveProperty("completed")
         expect(res.body).toHaveProperty("id")
